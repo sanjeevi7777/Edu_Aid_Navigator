d63916d7d08bbeda8c6e3e8b779ee348f34830b0
sanjeevi7777 <104307183+sanjeevi7777@users.noreply.github.com>
2024-01-22 15:33:06 +0530

    adding_files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Hello_World from "./components/Hello_World"
+
+function App() {
+  return (
+    <>
+     <Hello_World/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Hello_World.jsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+function Hello_World() {
+    return ( <div>
+        Hello World
+    </div> );
+}
+
+export default Hello_World;
